fed29b790bc9ec70a0bed0540482e0c711b6ddd7
GBTC440011UR <GBTC440011UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-02 19:37:30 -0400

    SpringBoot202_Ex

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+public class Student {
+    private long id;
+    private String name;
+    private int age;
+    private String height;
+    private int phone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
